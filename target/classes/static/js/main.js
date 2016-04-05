d94885b36c6adc165caa364c88603b24272b2ee2
Steven Liao <steven_liao@brown.edu>
2016-04-05 11:45:11 -0400

    Finished zoom and pan functions in terms of changing coordinates. Now we just need to be able to paint the board given two pairs of LatLng coordinates.

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -16,12 +16,20 @@ var canvas = document.getElementById("map");
 var ctx = canvas.getContext("2d");
 
 // setting the width and height will be tricky tricky shmumpkin shmicky
-ctx.canvas.width = canvas.width;
-ctx.canvas.height = canvas.height;
+ctx.canvas.width = this.innerWidth;
+ctx.canvas.height = this.innerHeight;
 
 //arbitrarily we set all drawn lines to Red for now.
 ctx.fillStyle = "#FF0000";
 ctx.strokeStyle = "#FF0000";
+
+// Is the mouse currently pressed down? Used for panning
+var isDown = false;
+
+// The previous position (in x and y coordinates) of the mouse. 
+// Used for panning
+var last_position = {};
+
 
 // The idea is we re-set the top left and top right coordinates,
 // and then re-draw the map using a function that only takes in those coordinates.
@@ -38,13 +46,14 @@ ctx.strokeStyle = "#FF0000";
 // We decided to copy that as a cool swag functionality.
 // How it works is wherever the cursor is pointing on a map, if you zoom,
 // the cursor will still be pointing at the same thing.
-// so if you zoom out to the U.S., and you put your cursor on LA 
+// so if you zoom out to the U.S., and then you put your cursor on LA 
 // when you zoom all the way in, your cursor will still be on LA
 $("#map").on('mousewheel', function(e) {
 	
   // zoomExp will equal 1 if the user scrolls up,
   // and -1 if the user scrolls down
   var zoomExp = e.originalEvent.wheelDelta/120;
+  
   // zoomFactor will equal 1.5 if the user scrolls up,
   // and 1/1.5 = 2/3 if the user scrolls down
   var zoomFactor = Math.pow(1.5, zoomExp);
@@ -58,10 +67,10 @@ $("#map").on('mousewheel', function(e) {
   
     // 1. [new bRLat] - [new tLLat] = zoomFactor*([old bRLat] - [old tLLat])
     // 2. [new tLLong] - [new bRLong] = zoomFactor*([old tLLong] - [old bRLong])
-    // 3. (Px - tLLat)/(brLat - tLLat) stays constant
-    // 4. (Py - bRLong)/(tLLong - brLong) stays constant
+    // 3. (P_lat - tLLat)/(brLat - tLLat) stays constant
+    // 4. (P_long - bRLong)/(tLLong - brLong) stays constant
     //
-    // where Px, Py is the position of the cursor when zooming
+    // where P_lat and P_long refer to the coordinates the mouse is pointing at
   
     //geographic length of map
     var length = brLat - tLLat;
@@ -72,34 +81,89 @@ $("#map").on('mousewheel', function(e) {
     // leftmost and topmost coordinates of the canvas
     var cursorX = e.pageX - this.offsetLeft;
     var cursorY = e.pageY - this.offsetTop;
-	  
-  }
-  
-  
-  
-  
-  
-  var exp = e.originalEvent.wheelDelta / 120;
-  var fillFactor = Math.pow(2, exp);
-  ctx.clearRect(tlX, tlY, percFilled, percFilled);
-  percFilled = percFilled * fillFactor;
-  ctx.fillRect(tlX, tlY, percFilled, percFilled);
+    
+    // what fraction of the total canvas width is the x value?
+    // what fraction of the total canvas height is the y value?
+    var proportionLength = cursorX / this.innerWidth;
+    var proportionHeight = cursorY / this.innerHeight;
+    
+    // the latitude and longitude of the cursor point
+    var cursorLat = proportionLength * length + tLLat;
+    var cursorLong = proportionLength * height + bRLong;
+    
+    // So (cursorLat - tLLat)/length should not change after zooming.
+    // We know length changes by a factor of zoomFactor
+    // and cursorLat doesn't change.
+    // Thus we can write
+    // 
+    //    (cursorLat - new_tLLat)/(zoomFactor * length) = 
+    // 	                            (cursorLat - old_tLLat)/ length
+    // -> (cursorLat - new_tLLat)/zoomFactor = cursorLat - old_ tLLat
+    // -> cursorLat - new_tLLat = zoomFactor*(cursorLat - old_tLLat)
+    // -> - new_tLLat = cursorLat(zoomFactor - 1) - zoomFactor*old_tLLat
+    // -> new_tLLat = cursorLat(1 - zoomFactor) + zoomFactor*old_tLLat
+    var newTLLat = cursorLat*(1 - zoomFactor) + zoomFactor*tLLat;
+    var newBRLat = newTLLat + length*zoomFactor;
+    
+    // similar calculations are done to find newTLLong and newBRLong
+    var newBRLong = cursorLong*(1 - zoomFactor) + zoomFactor*BRLong;
+    var newTLLong = newBRLong + height*zoomFactor;
+    
+    tLLat = newTLLat;
+    tLLong = newTLLong;
+    bRLat = newBRLat;
+    brLong = newBRLong;
+    
+    zoom += zoomExp;    
+    // re-paint-canvas with new coordinates!  
+  }  
 });
 
 $("#map").on('mousedown', function(e) {
   isDown = true;
-  var x = e.pageX - this.offsetLeft;
-  var y = e.pageY - this.offsetTop;
-  ctx.moveTo(x, y);
+  last_position = { x: e.pageX, y: e.pageY}
 });
 
 $("#map").on('mousemove', function(e) {
+  
+  // only do stuff if mouse is currently pressed down
   if (!isDown) return;
-  var x = e.pageX - this.offsetLeft;
-  var y = e.pageY - this.offsetTop;
-  ctx.lineTo(x, y);
-  ctx.stroke();
-})
+  
+  // we want to access the previous x and y value. 
+  // we check that they're not undefined.
+  
+  // (though they should never be undefined when 
+  // isDown is true, since isDown is only true when 
+  // the mouse is pressed down)
+  if ((typeof(last_position.x) != 'undefined') &
+		  (typeof(last_position.y) != 'undefined')){
+	  
+	  // change in x and y values
+	  var deltaX = e.pageX - last_position.x;
+	  var deltaY = e.pageY - last_position.y;
+	  
+	  // what fraction of the total canvas width is the deltaX?
+	  // what fraction of the total canvas height is the deltaY?
+	  var proportionWidth = deltaX / this.innerWidth;
+	  var proportionHeight = deltaY / this.innerHeight;
+	  
+	  //geographic length of map
+	  var length = brLat - tLLat;
+	  // geographic height of map
+	  var height = tLLong - bRLong;
+	  
+	  var deltaLat = length*proportionWidth;
+	  var deltaLong = height*proportionHeight;
+	  
+	  tLLat += deltaLat;
+	  tLLong += deltaLong;
+	  bRLat += deltaLat;
+	  bRLong += deltaLong;
+	  
+	  // re-paint canvas with new coordinates!
+  }
+  last_position = {x: e.pageX, y: e.pageY};
+  })
 
 $("#map").on('mouseup', function(e) {
   isDown = false;
